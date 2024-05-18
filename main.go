44a58a5daab205c558bbc94e8614c5b0684e3f52
fantasyczl <czilong0618@gmail.com>
2024-05-18 14:56:35 +0800

    feature: repl

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/user"
 
+	"github.com/fantasyczl/monkey/repl"
+)
+
+func main() {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", u.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
