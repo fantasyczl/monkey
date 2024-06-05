7288d06e5471f0de048d39166ba7000daeea7c33
fantasyczl <czilong0618@gmail.com>
2024-06-05 22:54:35 +0800

    feature: support Modify in ast

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -64,6 +64,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		body := node.Body
 		return &object.Function{Parameters: params, Body: body, Env: env}
 	case *ast.CallExpression:
+		if node.Function.TokenLiteral() == "quote" {
+			return quote(node.Arguments[0])
+		}
+
 		function := Eval(node.Function, env)
 		if isError(function) {
 			return function
@@ -488,4 +492,8 @@ func evalHashIndexExpression(hash, index object.Object) object.Object {
 	}
 
 	return pair.Value
+}
+
+func quote(node ast.Node) object.Object {
+	return &object.Quote{Node: node}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -526,3 +526,54 @@ func TestHashIndexExpressions(t *testing.T) {
 		}
 	}
 }
+
+func TestQuote(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{`quote(5)`, "5"},
+		{`quote(5 + 8)`, "(5 + 8)"},
+		{`quote(foobar)`, "foobar"},
+		{`quote(foobar + barfoo)`, "(foobar + barfoo)"},
+		{`quote([1, 2, 3])`, "[1, 2, 3]"},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		quote, ok := evaluated.(*object.Quote)
+		if !ok {
+			t.Fatalf("object is not Quote. got=%T (%+v)", evaluated, evaluated)
+		}
+		if quote.Node == nil {
+			t.Fatalf("quote.Node is nil")
+		}
+		if quote.Node.String() != tt.expected {
+			t.Fatalf("quote.Node.String() is not %q. got=%q", tt.expected, quote.Node.String())
+		}
+	}
+}
+
+func TestQuoteUnquote(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{`quote(unquote(4))`, "4"},
+		{`quote(unquote(4 + 4))`, "8"},
+		{`quote(8 + unquote(4))`, "(8 + 4)"},
+		{`quote(unquote(4 + 4) + 8)`, "(8 + 8)"},
+	}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		quoteTest, ok := evaluated.(*object.Quote)
+		if !ok {
+			t.Fatalf("object is not Quote. got=%T (%+v)", evaluated, evaluated)
+		}
+		if quoteTest.Node == nil {
+			t.Fatalf("quote.Node is nil")
+		}
+		if quoteTest.Node.String() != tt.expected {
+			t.Fatalf("quote.Node.String() is not %q. got=%q", tt.expected, quoteTest.Node.String())
+		}
+	}
+}
