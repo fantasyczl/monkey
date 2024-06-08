3a2c84366da7dd9c691dc3247f32c47232721fd3
fantasyczl <czilong0618@gmail.com>
2024-06-08 18:05:33 +0800

    feature: unquote

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -562,6 +562,30 @@ func TestQuoteUnquote(t *testing.T) {
 		{`quote(unquote(4 + 4))`, "8"},
 		{`quote(8 + unquote(4))`, "(8 + 4)"},
 		{`quote(unquote(4 + 4) + 8)`, "(8 + 8)"},
+		{
+			`let foobar=8;quote(foobar)`,
+			`foobar`,
+		},
+		{
+			`let foobar=8;quote(unquote(foobar))`,
+			`8`,
+		},
+		{
+			`quote(unquote(true))`,
+			`true`,
+		},
+		{
+			`quote(unquote(true == false))`,
+			`false`,
+		},
+		{
+			`quote(unquote(quote(4 + 4)))`,
+			`(4 + 4)`,
+		},
+		{
+			`let quotedInfixExpression = quote(4 + 4); quote(unquote(4 + 4) + unquote(quotedInfixExpression))`,
+			`(8 + (4 + 4))`,
+		},
 	}
 	for _, tt := range tests {
 		evaluated := testEval(tt.input)
