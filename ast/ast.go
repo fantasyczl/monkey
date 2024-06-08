77ee5eb347af5d33c661167c8a45884efccd4b3f
fantasyczl <czilong0618@gmail.com>
2024-06-08 21:10:07 +0800

    feature: parse macro

--- ast/ast.go
+++ ast/ast.go
@@ -475,3 +475,32 @@ func Modify(node Node, modifier ModifierFunc) Node {
 
 	return modifier(node)
 }
+
+type MacroLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (ml *MacroLiteral) expressionNode() {}
+
+func (ml *MacroLiteral) TokenLiteral() string {
+	return ml.Token.Literal
+}
+
+func (ml *MacroLiteral) String() string {
+	var out bytes.Buffer
+
+	params := make([]string, 0, len(ml.Parameters))
+	for _, p := range ml.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(ml.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(")")
+	out.WriteString(ml.Body.String())
+
+	return out.String()
+}
